a837faf31eef3963a99acd9b841a2ae0e547c5a9
ennzudev <dev@ennzu.com>
2023-08-13 03:57:12 -0600

    ✨ Basic html files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import SideBarNavigation from "./components/SideBarNavigation";
+
+function App() {
+    return (
+        <div className='flex bg-white w-screen h-screen'>
+            <SideBarNavigation />
+        </div>
+    );
+}
+export default App;
